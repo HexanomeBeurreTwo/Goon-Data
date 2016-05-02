febbd7db5e0f74a786279b5d1d1aa536faeca53f
V1C0D3R <V1C0D3R@gmail.com>
2016-05-02 19:53:50 +0200

    Add sequelize required files to project

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ function launchUpdateData(done)
 		var normilizedEvent = normalize[0](data);
 
 		// logActivity(normilizedEvent);
-		// insertion.insertAllActivities(normilizedEvent);
+		insertion.insertAllActivities(normilizedEvent);
 		// insertion.insertActivity(normilizedEvent[0]);
 		console.log('GrandLyon Data Updated at : ' + new Date());
 
